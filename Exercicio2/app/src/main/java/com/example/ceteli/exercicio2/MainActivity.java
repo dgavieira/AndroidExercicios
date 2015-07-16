e3d36360023e1b905ffd1f835f535755ddc60a6d
dgavieira <dialcantaravieira@hotmail.com>
2015-07-15 20:50:09 -0400

    Exercício 3 - Tratando Eventos
    Com erro para corrigir em passar dados de uma activity para outra
    através de uma intent

--- Exercicio2/app/src/main/java/com/example/ceteli/exercicio2/MainActivity.java
+++ Exercicio2/app/src/main/java/com/example/ceteli/exercicio2/MainActivity.java
@@ -6,8 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,16 +26,66 @@ public class MainActivity extends ActionBarActivity {
         final EditText mail = (EditText) findViewById(R.id.edtTxtMail);
         final EditText salario = (EditText) findViewById(R.id.edtTxtSalario);
         final Button confirma = (Button) findViewById(R.id.btSend);
+        final RadioGroup estadocivil = (RadioGroup) findViewById(R.id.rdGp1);
+        final RadioButton casado = (RadioButton) findViewById(R.id.rdBtCasado);
+        final RadioButton solteiro = (RadioButton) findViewById(R.id.rdBtSolteiro);
+        final RadioButton divorciado = (RadioButton) findViewById(R.id.rdBtDivorciado);
+        final CheckBox checkmail = (CheckBox) findViewById(R.id.ckBoxeMail);
+        final CheckBox sms = (CheckBox) findViewById(R.id.ckBoxSMS);
+        final CheckBox wpp = (CheckBox) findViewById(R.id.ckBoxWhatsApp);
+        final ToggleButton toggle = (ToggleButton) findViewById(R.id.tgBtCadastro);
+        final String[] msg = new String[1];;
+        final String[]  String[][];
+        String[][] msgemail;
 
-        confirma.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mensagem = " ";
-                mensagem+= nome.getText().toString() + " \n";
-                mensagem+= fone.getText().toString() + " \n";
-                mensagem+= mail.getText().toString() + " \n";
-                mensagem+= salario.getText().toString() + " \n";
-                Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+        msgemail = new String[1][1];
+        ;
+        final String[] msgsms = new String[1];
+        final String msgwpp;
+
+        toggle.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View v){
+                if (((ToggleButton) v).isChecked()){
+                    estadocivil.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(RadioGroup group, int checkedId) {
+                            if (casado.isChecked()) {
+                                msg[0] = "casado";
+                            }
+                            if (solteiro.isChecked()) {
+                                msg[0] = "solteiro";
+                            }
+                            if (divorciado.isChecked()) {
+                                msg[0] = "divorciado";
+                            }
+                        }
+                    });
+
+                    confirma.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String mensagem = " ";
+                            mensagem+= nome.getText().toString() + " \n";
+                            mensagem+= fone.getText().toString() + " \n";
+                            mensagem+= mail.getText().toString() + " \n";
+                            mensagem+= salario.getText().toString() + " \n";
+                            mensagem+= msg[0] + "\n";
+                            mensagem+= msgsms[0] + "\n";
+                            mensagem+= msgwpp + "\n";
+
+                            Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                else{
+                    confirma.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String mensagem = "Cadastro Inativo";
+                            Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
         });
     }
